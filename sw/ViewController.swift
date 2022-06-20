fe6f4037407ffc8d49bef9c30be89f0f1e7d700f
xianwei.yin <xianwei.yin@mei1.com>
2022-06-20 14:33:00 +0800

    Initial Commit

---
+++ sw/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sw
+//
+//  Created by yinxianwei on 2022/6/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
